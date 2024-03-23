ee4178865258a3247c9a9d0edad64b5873a7a5ef
edylim <edylim@users.noreply.github.com>
2024-03-23 15:39:32 -0700

    Add copilot and copilot-cmp

--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -3,6 +3,48 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    require("copilot").setup({})
+    require("copilot").setup({
+      panel = {
+        enabled = false, -- for copilot_cmp, the rest of the options are ignored
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.2,
+        },
+      },
+      suggestion = {
+        enabled = false, -- for copilot_cmp, the rest of the options are ignored
+        auto_trigger = true,
+        debounce = 150,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
   end,
 }
--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -13,6 +13,13 @@ return {
     local cmp = require("cmp")
     local lspkind = require("lspkind")
     local luasnip = require("luasnip")
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
 
     require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -36,18 +43,46 @@ return {
           cmp.mapping.abort()
         end),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
       }),
       sources = cmp.config.sources({
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
         { name = "buffer" },
-        { name = "luasnip" },
-        { name = "nvim_lsp" },
-        { name = "path" },
       }),
       formatting = {
         format = lspkind.cmp_format({
-          maxwidth = 50,
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = { Copilot = "" },
           ellipsis_char = "...",
         }),
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
       },
     })
   end,
